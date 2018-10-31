8d8c0934935b99be311cbec07cfc635bf44dccc0
keyur2714 <keyur.555kn@gmail.com>
2018-10-31 09:57:25 +0530

    Added Template Driven Forms ... With more Controls

--- TemplateDrivenFormsDemo/src/app/student-entry/student-entry.component.ts
+++ TemplateDrivenFormsDemo/src/app/student-entry/student-entry.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Student } from './student.model';
+import { Course } from './course.model';
 @Component({
   selector: 'app-student-entry',
   templateUrl: './student-entry.component.html',
@@ -8,17 +9,46 @@ import { Student } from './student.model';
 export class StudentEntryComponent implements OnInit {
 
   student : Student = new Student();
-  isSubmitted :boolean = false;
+  isSubmitted :boolean = false;  
+
+  courseList : Course[] = [];
 
   constructor() { }
 
   ngOnInit() {
     this.student.rollNo = 1;
+    this.student.gender = 'M';
+
+    let course1 = new Course();
+    course1.id = 10;
+    course1.name = "Angular";
+    course1.fees =12000;
+    course1.trainerName = "Keyur";
+
+    let course2 = new Course();
+    course2.id = 20;
+    course2.name = "BigData";
+    course2.fees =15000;
+    course2.trainerName = "Vaibhav";
+
+    let course3 = new Course();
+    course3.id = 30;
+    course3.name = "Java";
+    course3.fees =10000;
+    course3.trainerName = "Keyur";
+
+    this.courseList.push(course1);
+    this.courseList.push(course2);
+    this.courseList.push(course3);
+
+    this.student.course = this.courseList[1];
+    
   }
 
   save(){
     console.log(this.student.rollNo);
-    this.isSubmitted = true;
+    console.log(this.student.refferedBy);
+    this.isSubmitted = true;    
   }
 
 }
--- TemplateDrivenFormsDemo/src/app/student-entry/student.model.ts
+++ TemplateDrivenFormsDemo/src/app/student-entry/student.model.ts
@@ -1,3 +1,4 @@
+import { Course } from './course.model';
 export class Student{
-    constructor(public rollNo?:number,name?:string,mobileNo?:string,email?:string,gender?:string,course?:string,refferedBy?:string){}
+    constructor(public rollNo?:number,public name?:string,public mobileNo?:string,public email?:string,public gender?:string,public course?:Course,public refferedBy?:string){}
 }
